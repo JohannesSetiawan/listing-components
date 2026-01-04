e7e7b35c1c2e8d34e836fd80a79918bb8a6db417
yohaneswijaya12 <johannes.setiawan@ui.ac.id>
2026-01-04 18:03:58 +0700

    [fix] Combines API client list & detail

--- src/pages/__init__.py
+++ src/pages/__init__.py
@@ -2,5 +2,4 @@
 from src.pages.home import render_home
 from src.pages.find_dm_links import render_find_dm_links
 from src.pages.audit_trail import render_audit_trail
-from src.pages.api_client_list import render_api_client_list
-from src.pages.api_client_detail import render_api_client_detail
+from src.pages.api_client_detail import render_api_client_page
